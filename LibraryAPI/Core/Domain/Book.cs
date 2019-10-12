9c0a681aa59835973f4645ce5e9ef5a9c79b4c01
Vijay Yadav <vijay42u@gmail.com>
2019-10-12 12:35:52 +0000

    added book

--- LibraryAPI/Core/Domain/Book.cs
+++ LibraryAPI/Core/Domain/Book.cs
@@ -9,14 +9,14 @@ namespace LibraryAPI.Core.Domain
     public class Book
     {
         public string bookName { get; set; }
-        public string bookId { get; set; }
+        public Guid Id { get; set; }
         public string userId { get; set; }
 
-        public static Book AddBook(string name, string bookId,string userId)
+        public static Book AddBook(string name,string userId)
         {
             Book addBook = new Book();
             addBook.bookName = name;
-            addBook.bookId = bookId;
+            addBook.Id = Guid.NewGuid();
             addBook.userId = userId;
 
             return addBook;
