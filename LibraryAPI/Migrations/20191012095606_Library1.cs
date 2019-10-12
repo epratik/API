0bc665ea8409472173d0f9b053274b73f4bb778e
Sonu Kumar <sonu.sri@outlook.com>
2019-10-12 10:02:05 +0000

    DB Connection

---
+++ LibraryAPI/Migrations/20191012095606_Library1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryAPI.Migrations
+{
+    public partial class Library1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Users");
+        }
+    }
+}
